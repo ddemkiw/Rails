8bb957f0e2e7ccb738ff361acf60625f82ebedc1
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-17 12:35:42 +0000

    Addition of first restaurant in db with test

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+  
   context 'no restos added' do
     scenario 'should display prompt for resto addition' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants'
       expect(page).to have_link 'Add a restaurant'
     end
+  end
+
+  context 'restaurants have been added' do
+    
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restos' do
+      visit '/restaurants'
+      expect(page).to have_content 'KFC'
+      expect(page).not_to have_content 'No restaurants'
+    end
+
   end
 end
